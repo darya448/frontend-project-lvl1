aec3f0cf125c921ccdf7d608fbcb707354d20097
Darya <daryachzhan@gmail.com>
2021-06-03 14:48:17 +0300

    fixed

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-
-const knowname = name();
-
-console.log(`Hello,${knowname}!`);
+console.log('Hello, Tirion');
